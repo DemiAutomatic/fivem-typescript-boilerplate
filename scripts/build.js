2067439969e41826b07eb2ba7411d1ed57130075
DemiAutomatic <austindunn.dev@gmail.com>
2025-12-29 10:58:53 -0700

    remove web from sqlite branch

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,9 @@
 //@ts-check
 
-import { exists, exec, getFiles } from "./utils.js";
+import { exec, getFiles } from "./utils.js";
 import { createBuilder, createFxmanifest } from "@communityox/fx-utils";
 
 const watch = process.argv.includes("--watch");
-const web = await exists("./web");
 const dropLabels = ["$BROWSER"];
 
 if (!watch) dropLabels.push("$DEV");
@@ -37,22 +36,18 @@ createBuilder(
       },
     },
   ],
+
   async (outfiles) => {
-    const files = await getFiles("dist/web", "static", "locales");
+    const files = await getFiles("static", "locales");
     await createFxmanifest({
       client_scripts: [outfiles.client],
       server_scripts: [outfiles.server],
       files: ["lib/init.lua", "lib/client/**.lua", "locales/*.json", ...files],
       dependencies: ["/server:13068", "/onesync", "oxmysql", "ox_lib"],
       metadata: {
-        ui_page: "dist/web/index.html",
         lua54: "yes",
         node_version: "22",
       },
     });
-
-    if (web && !watch) await exec("cd ./web && vite build");
   },
 );
-
-if (web && watch) await exec("cd ./web && vite build --watch");
