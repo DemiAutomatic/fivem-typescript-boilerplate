138fa14d44769307a3d2a5d1e6dd71196e88f840
DemiAutomatic <austindunn.dev@gmail.com>
2025-12-29 13:44:37 -0700

    Remove oxmysql dependency

--- scripts/build.js
+++ scripts/build.js
@@ -43,7 +43,7 @@ createBuilder(
       client_scripts: [outfiles.client],
       server_scripts: [outfiles.server],
       files: ["lib/init.lua", "lib/client/**.lua", "locales/*.json", ...files],
-      dependencies: ["/server:13068", "/onesync", "oxmysql", "ox_lib"],
+      dependencies: ["/server:13068", "/onesync", "ox_lib"],
       metadata: {
         ui_page: "dist/web/index.html",
         lua54: "yes",
